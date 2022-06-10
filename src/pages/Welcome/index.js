c1df8970b69f43d3e0214311d84c5379f931800a
Jarrete Barnett <jarretebarnett@gmail.com>
2022-06-10 01:23:28 -0400

    Refining structure and CSS of Welcome landing page.

--- src/pages/Welcome/index.js
+++ src/pages/Welcome/index.js
@@ -12,8 +12,6 @@ function Welcome() {
             <br />
             <br />
             <br />
-            <br />
-            <br />
             <h1 className="slide-one"><span className="badge bg-black">Full-Stack Software Engineer</span></h1>
             <h1 className="slide-two"><span className="badge bg-black">Licensed Real Estate Sales Associate</span></h1>
             <h1 className="slide-three"><span className="badge bg-black">Entrepreneur</span></h1>
