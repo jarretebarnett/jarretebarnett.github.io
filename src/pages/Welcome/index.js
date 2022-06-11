d21093ab37477c7e1229ec59e2f54bccceb8f6c4
Jarrete Barnett <jarretebarnett@gmail.com>
2022-06-11 01:39:40 -0400

    Refining fade animation on Welcome page.

--- src/pages/Welcome/index.js
+++ src/pages/Welcome/index.js
@@ -16,7 +16,7 @@ function Welcome() {
             <h1 className="slide-two"><span className="badge bg-black">Licensed Real Estate Sales Associate</span></h1>
             <h1 className="slide-three"><span className="badge bg-black">Entrepreneur</span></h1>
             <br />
-            <p className="statement" behavior="alternate" direction="up">Welcome to an exploratory documentation of prior and current experience related to time spent working within the tech industry. Choose from the menu to discover constructed projects, repositories and social media access.</p>
+            <p className="statement" behavior="alternate" direction="up">This is an exploratory documentation of prior and current experience relative to time spent working within the tech industry. Choose from the menu to discover compiled project stacks, repositories and social media access.</p>
         </div>
     );
 }
