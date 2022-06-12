b203dc8c8b204b2bce5fdb7fde63ed31bea4048f
Jarrete Barnett <jarretebarnett@gmail.com>
2022-06-12 18:42:07 -0400

    Adding glow effect to navbar and link titles on Portfolio page.

--- src/components/Nav/index.js
+++ src/components/Nav/index.js
@@ -4,11 +4,11 @@ import React from 'react';
 function Nav() {
     return (
         <nav className="navbar">
-            <span className="label"><i className=""></i>Jarreté Barnett</span>
+            <span id="title-glow" className="label"><i className=""></i>Jarreté Barnett</span>
             <ul className="items">
-                <li><a className="nav-link" href="/#/experience">Experience</a></li>
-                <li><a className="nav-link" href="/#/portfolio">Portfolio</a></li>
-                <li><a className="nav-link" href="/#/contact">Contact</a></li>
+                <li><a id="title-glow" className="nav-link" href="/#/experience">Experience</a></li>
+                <li><a id="title-glow" className="nav-link" href="/#/portfolio">Portfolio</a></li>
+                <li><a id="title-glow" className="nav-link" href="/#/contact">Contact</a></li>
                 {/* <li><a className="nav-link" href={certs} rel="noreferrer" target="_blank">Certs</a></li> */}
             </ul>
         </nav>
--- src/pages/Portfolio/index.js
+++ src/pages/Portfolio/index.js
@@ -15,46 +15,46 @@ function Portfolio() {
             <br />
             <span id="slider" className="badge bg-black">
                 <div id="carouselControls" className="carousel slide carousel-fade" data-ride="carousel">
-                <ol className="carousel-indicators">
-                    <li data-target="#carouselIndicators" data-slide-to="0" className="active"></li>
-                    <li data-target="#carouselIndicators" data-slide-to="1"></li>
-                    <li data-target="#carouselIndicators" data-slide-to="2"></li>
-                    <li data-target="#carouselIndicators" data-slide-to="3"></li>
-                    <li data-target="#carouselIndicators" data-slide-to="4"></li>
-                </ol>
+                    <ol className="carousel-indicators">
+                        <li data-target="#carouselIndicators" data-slide-to="0" className="active"></li>
+                        <li data-target="#carouselIndicators" data-slide-to="1"></li>
+                        <li data-target="#carouselIndicators" data-slide-to="2"></li>
+                        <li data-target="#carouselIndicators" data-slide-to="3"></li>
+                        <li data-target="#carouselIndicators" data-slide-to="4"></li>
+                    </ol>
                     <div className="carousel-inner">
                         <div className="carousel-item active">
                             <img className="d-block w-100" src={trekgambit} alt="First slide"/>
                             <div className="carousel-caption d-none d-md-block">
-                                <h1><span className="badge bg-black"><a href="https://quiet-harbor-96544.herokuapp.com/" rel="noreferrer" target="_blank">Trek Gambit </a><a href="https://github.com/jarretebarnett/trekgambit" rel="noreferrer" target="_blank"><img src={githublogo} style={{ maxWidth: 22 }} alt=""/></a></span></h1>
+                                <h1><span className="badge bg-black"><a id="title-glow" href="https://quiet-harbor-96544.herokuapp.com/" rel="noreferrer" target="_blank">Trek Gambit </a><a href="https://github.com/jarretebarnett/trekgambit" rel="noreferrer" target="_blank"><img id="logo-glow" src={githublogo} style={{ maxWidth: 22 }} alt=""/></a></span></h1>
                                 <h4><span className="badge bg-black">This application is designed to help search for national park information.</span></h4>
                             </div>
                         </div>
                         <div className="carousel-item">
                             <img className="d-block w-100" src={googlebooks} alt="Second slide"/>
                             <div className="carousel-caption d-none d-md-block">
-                                <h1><span className="badge bg-black"><a href="https://bookapigoogle.herokuapp.com/" rel="noreferrer" target="_blank">Google Books API Queue </a><a href="https://github.com/jarretebarnett/googlebooks" rel="noreferrer" target="_blank"><img src={githublogo} style={{ maxWidth: 22 }} alt=""/></a></span></h1>
+                                <h1><span className="badge bg-black"><a id="title-glow" href="https://bookapigoogle.herokuapp.com/" rel="noreferrer" target="_blank">Google Books API Queue </a><a href="https://github.com/jarretebarnett/googlebooks" rel="noreferrer" target="_blank"><img src={githublogo} style={{ maxWidth: 22 }} alt=""/></a></span></h1>
                                 <h4><span className="badge bg-black">An Axios API query which returns book searches and saves to a database.</span></h4>
                             </div>
                         </div>
                         <div className="carousel-item">
                             <img className="d-block w-100" src={betaportfolio} alt="Third slide"/>
                             <div className="carousel-caption d-none d-md-block">
-                                <h1><span className="badge bg-black"><a href="https://jarretebarnett.github.io/reactportfolio/" rel="noreferrer" target="_blank">Portfolio | Beta Version </a><a href="https://github.com/jarretebarnett/reactportfolio" rel="noreferrer" target="_blank"><img src={githublogo} style={{ maxWidth: 22 }} alt=""/></a></span></h1>
+                                <h1><span className="badge bg-black"><a id="title-glow" href="https://jarretebarnett.github.io/reactportfolio/" rel="noreferrer" target="_blank">Portfolio | Beta Version </a><a href="https://github.com/jarretebarnett/reactportfolio" rel="noreferrer" target="_blank"><img src={githublogo} style={{ maxWidth: 22 }} alt=""/></a></span></h1>
                                 <h4><span className="badge bg-black">Early beta version portfolio utilizing React.</span></h4>
                             </div>
                         </div>
                         <div className="carousel-item">
                             <img className="d-block w-100" src={lyrichord} alt="Fourth slide"/>
                             <div className="carousel-caption d-none d-md-block">
-                                <h1><span className="badge bg-black"><a href="https://seanmonaghan.github.io/LyriChord/" rel="noreferrer" target="_blank">LyriChord </a><a href="https://github.com/jarretebarnett/LyriChord" rel="noreferrer" target="_blank"><img src={githublogo} style={{ maxWidth: 22 }} alt=""/></a></span></h1>
+                                <h1><span className="badge bg-black"><a id="title-glow" href="https://seanmonaghan.github.io/LyriChord/" rel="noreferrer" target="_blank">LyriChord </a><a href="https://github.com/jarretebarnett/LyriChord" rel="noreferrer" target="_blank"><img src={githublogo} style={{ maxWidth: 22 }} alt=""/></a></span></h1>
                                 <h4><span className="badge bg-black">Utilizes 2-3 API functions to deliver musical amusements.</span></h4>
                             </div>
                         </div>
                         <div className="carousel-item">
                             <img className="d-block w-100" src={weatherdash} alt="Fifth slide"/>
                             <div className="carousel-caption d-none d-md-block">
-                                <h1><span className="badge bg-black"><a href="https://jarretebarnett.github.io/weatherdashboard/" rel="noreferrer" target="_blank">Weather API Dashboard </a><a href="https://github.com/jarretebarnett/weatherdashboard" rel="noreferrer" target="_blank"><img src={githublogo} style={{ maxWidth: 22 }} alt=""/></a></span></h1>
+                                <h1><span className="badge bg-black"><a id="title-glow" href="https://jarretebarnett.github.io/weatherdashboard/" rel="noreferrer" target="_blank">Weather API Dashboard </a><a href="https://github.com/jarretebarnett/weatherdashboard" rel="noreferrer" target="_blank"><img src={githublogo} style={{ maxWidth: 22 }} alt=""/></a></span></h1>
                                 <h4><span className="badge bg-black">A dashboard page utilizing the OpenWeatherMap API.</span></h4>
                             </div>
                         </div>
