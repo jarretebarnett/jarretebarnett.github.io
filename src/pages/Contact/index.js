f6638740ef09f7d0afea101c2a747c2152c1cfaa
Jarrete Barnett <jarretebarnett@gmail.com>
2022-06-13 20:32:11 -0400

    Commiting changes, including completion of Contact page.

--- src/pages/Contact/index.js
+++ src/pages/Contact/index.js
@@ -1,4 +1,11 @@
 import React from 'react';
+import githublogo from "../../assets/img/githublogo.png";
+import linkedin from "../../assets/img/linkedin.png";
+import facebook from "../../assets/img/facebook.png";
+import twitter from "../../assets/img/twitter.png";
+import instagram from "../../assets/img/instagram.jpg";
+import youtube from "../../assets/img/youtube.png";
+import twitch from "../../assets/img/twitch.png";
 
 function Contact() {
     return (
@@ -11,9 +18,55 @@ function Contact() {
                 <div className="row">
                     <div id="colTitle" className="col-auto">
                         <p>Social Media</p>
+                        <div className="row">
+                            <a id="social-glow" className="lean-right" href="https://www.github.com/jarretebarnett/" rel="noreferrer" target="_blank">GitHub <img src={githublogo} style={{ maxWidth: 20 }} alt=""/></a>
+                        </div>
+                        <br />
+                        <div className="row">
+                            <a id="social-glow" className="lean-right" href="https://www.linkedin.com/in/jarretebarnett/" rel="noreferrer" target="_blank">LinkedIn <img src={linkedin} style={{ maxWidth: 20 }} alt=""/></a>
+                        </div>
+                        <br />
+                        <div className="row">
+                            <a id="social-glow" className="lean-right" href="https://www.facebook.com/jarrete.y.barnett/" rel="noreferrer" target="_blank">Facebook <img src={facebook} style={{ maxWidth: 20 }} alt=""/></a>
+                        </div>
+                        <br />
+                        <div className="row">
+                            <a id="social-glow" className="lean-right" href="https://twitter.com/jarrete_barnett" rel="noreferrer" target="_blank">Twitter <img src={twitter} style={{ maxWidth: 20 }} alt=""/></a>
+                        </div>
+                        <br />
+                        <div className="row">
+                            <a id="social-glow" className="lean-right" href="https://www.instagram.com/jarretebarnett/" rel="noreferrer" target="_blank">Instagram <img src={instagram} style={{ maxWidth: 20 }} alt=""/></a>
+                        </div>
+                        <br />
+                        <div className="row">
+                            <a id="social-glow" className="lean-right" href="https://www.youtube.com/channel/UCrvMqVxDXQxNlDNoavSRh5g" rel="noreferrer" target="_blank">YouTube <img src={youtube} style={{ maxWidth: 20 }} alt=""/></a>
+                        </div>
+                        <br />
+                        <div className="row">
+                            <a id="social-glow" className="lean-right" href="https://www.twitch.tv/jaewhybe" rel="noreferrer" target="_blank">Twitch <img src={twitch} style={{ maxWidth: 20 }} alt=""/></a>
+                        </div>
                     </div>
                     <div id="colTitle" className="col-auto">
                         <p>Communication</p>
+                        <div className="row">
+                            <p className="lean">m: 1-860-614-9019</p>
+                        </div>
+                        <br />
+                        <div className="row">
+                            <p className="lean">e: jarretebarnett<br/>@gmail.com</p>
+                        </div>
+                        <br />
+                        <div className="row">
+                            <p className="lean">w: jarrete.barnett<br/>@turnberrysolutions.com</p>
+                        </div>
+                        <br />
+                        <div className="row">
+                            <p className="lean">w: jarrete.barnett<br/>@mastercard.com</p>
+                        </div>
+                        <br />
+                        <div className="row">
+                            <p className="lean">w: jarrete.barnett<br/>@raveis.com</p>
+                        </div>
                     </div>
                 </div>
             </span></h1>
