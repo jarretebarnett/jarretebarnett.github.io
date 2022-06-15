d027b40b912651ef180729702475aa8ed49543a3
Jarrete Barnett <jarretebarnett@gmail.com>
2022-06-15 01:03:35 -0400

    Building out structure of Experience page.

--- src/pages/Experience/index.js
+++ src/pages/Experience/index.js
@@ -7,6 +7,59 @@ function Experience() {
             <br />
             <div className="page-title">Experience</div>
             <br />
+            <h1><span id="expWindow" className="badge bg-black">
+                <div>
+                    <p id="coName">Turnberry Solutions</p>
+                    <ul>
+                        <li id="clientName">Mastercard</li>
+                        <ul>
+                            <li id="posName">Software Engineer Associate I</li>
+                            <li id="expDate">May 2022 ⤬ Present</li>
+                            <ul>
+                                <li id="desc">⦿</li>
+                                <li id="desc">⦿</li>
+                                <li id="desc">⦿</li>
+                            </ul>
+                        </ul>
+                    </ul>
+                </div>
+                <div>
+                    <p id="coName">The Coder School</p>
+                    <ul>
+                        <li id="posName">Instructional Coach</li>
+                        <li id="expDate">November 2021 ⤬ April 2022</li>
+                        <ul>
+                            <li id="desc">⦿</li>
+                            <li id="desc">⦿</li>
+                            <li id="desc">⦿</li>
+                        </ul>
+                    </ul>
+                </div>
+                <div>
+                    <p id="coName">William Raveis Real Estate Inc.</p>
+                    <ul>
+                        <li id="posName">Licensed Real Estate Sales Associate</li>
+                        <li id="expDate">February 2020 ⤬ Present</li>
+                        <ul>
+                            <li id="desc">⦿</li>
+                            <li id="desc">⦿</li>
+                            <li id="desc">⦿</li>
+                        </ul>
+                    </ul>
+                </div>
+                <div>
+                    <p id="coName">Keller Williams Realty</p>
+                    <ul>
+                        <li id="posName">Licensed Real Estate Sales Associate</li>
+                        <li id="expDate">July 2019 ⤬ January 2020</li>
+                        <ul>
+                            <li id="desc">⦿</li>
+                            <li id="desc">⦿</li>
+                            <li id="desc">⦿</li>
+                        </ul>
+                    </ul>
+                </div>
+            </span></h1>
         </div>
     );
 }
